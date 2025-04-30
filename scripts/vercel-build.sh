d90342bce24c9ba8958c52281b102d80227c5dde
ox7f <ox7f@protonmail.com>
2025-04-30 13:39:07 +0200

    feat: add vercel build script

---
+++ scripts/vercel-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clone submodules with auth
+echo "Setting up submodules..."
+git submodule init
+git submodule update --remote --recursive
+
+# Git LFS pull
+echo "Fetching LFS files..."
+git lfs install
+cd public/sf-girls-assets
+git lfs pull --include="*" --recursive
+cd ../../
+
+# Proceed with the React build
+echo "Building React app..."
+npm install
+npm run build
